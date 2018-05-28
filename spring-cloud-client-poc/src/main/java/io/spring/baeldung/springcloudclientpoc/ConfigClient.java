1c682cdc936c51eea326187f3328f4bd24bc33e6
Gurminder Singh <gurminder.singh@bestbuy.com>
2018-05-28 15:24:40 -0500

    commit

--- spring-cloud-client-poc/src/main/java/io/spring/baeldung/springcloudclientpoc/ConfigClient.java
+++ spring-cloud-client-poc/src/main/java/io/spring/baeldung/springcloudclientpoc/ConfigClient.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ConfigClient {
 
-    @Value("${configuration.projectName}")
+    @Value("${user.role}")
     String userRole;
 
     public static void main(String[] args) {
